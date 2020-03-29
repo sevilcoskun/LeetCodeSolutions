706299ef3830ce5c921f5761845244ac86ffa93d
sevilcoskun <sevilcoskun7@gmail.com>
2020-03-29 19:24:04 +0200

    added 680th problem solution

---
+++ src/test/TestSolution680.java
@@ -0,0 +1,20 @@
+package test;
+
+import solutions.Solution680;
+import solutions.SolutionNumber;
+
+public class TestSolution680 {
+    public static void test(){
+        Solution680 s = new Solution680();
+        System.out.println(s.validPalindrome("aba"));
+
+        Solution680 s1 = new Solution680();
+        System.out.println(s1.validPalindrome("abca"));
+
+        Solution680 s2 = new Solution680();
+        System.out.println(s2.validPalindrome("abdca"));
+
+        Solution680 s3 = new Solution680();
+        System.out.println(s3.validPalindrome("abcdefghabcahgfedcba"));
+    }
+}
